a5c49148d134894dd99f8037d70d230af1dc93eb
LouisGaboury <louis.gaboury.4@gmail.com>
2022-02-02 15:54:05 +0100

    Updating querry when new one is entered

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,20 @@ export default function Home() {
 
   useNewSearch(query, pageNumber);
 
+  /**
+   * @description Change the querry according to the new one entered by the user
+   * @param {Object} event DOM event
+   */
+  const handleSearch = (event) => {
+    setQuery(event.target.value);
+    // Reset the page shown by default when a new search is entered
+    setPageNumber(1);
+  };
+
   return (
     <div className="m-8">
       <h1 className="text-4xl mb-4">Ma liste de news</h1>
-      <input type="text" className="border-2"></input>
+      <input type="text" onChange={handleSearch} className="border-2"></input>
     </div>
   );
 }
